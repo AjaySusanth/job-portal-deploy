f4d80b50e8ec4b3b43af64d49bf93024e2f9c9e6
Ajay Susanth <ajaysusanth10@gmail.com>
2025-01-08 03:11:56 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 const corsOptions = {
-    origin:'http://localhost:5000',
+    origin:'https://job-portal-live.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
